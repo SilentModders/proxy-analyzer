c6c9dab44e1b0e4cc2fb52bcaf80f9daaa4abc06
Andy Roxby <aroxby@users.noreply.github.com>
2018-11-03 16:30:46 -0400

    Create basic HTTP server for intercepting

---
+++ intercepting/server.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+import re
+import sys
+import traceback
+from socketserver import StreamRequestHandler
+from base_server import ServerProgram
+
+
+class Request(object):
+    def __init__(self, method, url, http_ver=None, headers=None, data=None):
+        self.method = method
+        self.url = url
+        self.http_ver = http_ver or 'HTTP/1.1'
+        self.headers = headers or {}
+        self.data = data
+        
+    def __str__(self):
+        data = '{} {} {}'.format(self.method, self.url, self.http_ver)
+        for key, value in self.headers.items():
+            data += '\n{}: {}'.format(key, value)
+        return data
+
+        
+class HTTPReader(object):
+    # FIXME: This all needs done with byte level IO
+    # There are several over performance offenses
+    max_header_lines = 32
+    max_data_lines = 1024
+    line_break = b'\r\n'
+    
+    def read_head(self, rfile):
+        request = b''
+        headers = 0
+        while(headers < self.max_header_lines):
+            line = rfile.readline()
+            # print(line, flush=True)
+            request += line
+            if not line or line == self.line_break:
+                break
+            headers += 1
+        return request
+            
+    def parse_method_line(self, line):
+        words = line.split(b' ')
+        if len(words) != 3:
+            raise ValueError('Invalid method line')
+       # TODO: Use namedtuple?
+        return {
+            'method': words[0].strip(),
+            'url': words[1].strip(),
+            'http_ver': words[2].strip(),
+        }
+        
+    def parse_header(self, header):
+        parts = header.split(b':', 1)
+        if len(parts) != 2:
+            raise ValueError('Invalid header')
+        # TODO: Use namedtuple?
+        return {
+            'key': parts[0].strip(),
+            'value': parts[1].strip(),
+        }
+        
+    def parse_head(self, data):
+        data = [_ for _ in data.split(self.line_break) if _]
+        if not data:
+            return None
+        method_line_data = self.parse_method_line(data[0])
+        method = method_line_data['method']
+        url = method_line_data['url']
+        http_ver = method_line_data['http_ver']
+        headers = {}
+        if len(data) > 1:
+            for header in data[1:]:
+                header_data = self.parse_header(header)
+                key = header_data['key'].lower()
+                value = header_data['value']
+                headers[key] = headers.get(key, []) + [value]
+        return Request(method, url, http_ver, headers)
+        
+        
+    def read_data(self, rfile):
+        data = b''
+        lines = 0
+        while(lines < self.max_data_lines):
+            line = rfile.readline()
+            data += line
+            if not line:
+                return data
+            lines += 1
+        
+    def read_request(self, rfile):
+        head = self.read_head(rfile)
+        request = self.parse_head(head)
+        if not request:
+            return None
+        length_headers = request.headers.get('content-length', [])
+        if length_headers:
+            if len(length_headers) > 1:
+                raise ValueError('"content-length" may only appear once')
+            else:
+                try:
+                    length = int(length_headers[0])
+                except ValueError:
+                    raise ValueError('Invalid "content-length"')
+                request.data = self.read_data(rfile)
+        return request
+
+
+class Handler(StreamRequestHandler):
+    canned_500 = b'HTTP 500 E\r\nConnection: Closed\r\n\r\nGoodbye'
+    def handle(self):
+        try:
+            request = HTTPReader().read_request(self.rfile)
+            print('\nRequest:')
+            print(str(request), flush=True)
+            self.wfile.write(self.canned_500)
+        finally:
+            sys.stderr.flush()
+
+
+class Server(ServerProgram):
+    handler = Handler
+
+
+if __name__ == '__main__':
+    sys.exit(Server.main(sys.argv))
