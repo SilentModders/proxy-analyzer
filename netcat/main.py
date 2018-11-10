0bc94de4f9d88d1c994df094d9488f5f3a21b207
Andy Roxby <aroxby@users.noreply.github.com>
2018-11-09 22:30:48 -0500

    Use BytesReader in netcat

--- netcat/main.py
+++ netcat/main.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 import sys
 from base_client import ClientProgram, ClientPipe
-from utils import IterStream, BytesWriter
-
-
-def byte_reader(stream):
-    def _wrapper(stream):
-        while True:
-            data = next(stream)
-            if data:
-                yield data.encode()
-
-    return IterStream(_wrapper(stream))
+from utils import BytesWriter, BytesReader
 
 
 def main(argv):
     url = argv[1]
-    pipe_in = byte_reader(sys.stdin)
+    pipe_in = BytesReader(sys.stdin)
     pipe_out = BytesWriter(sys.stdout)
 
     class Client(ClientProgram):
