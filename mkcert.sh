bfc45667323ca8a027441c76fd0fbdb6243a0040
Andy Roxby <aroxby@users.noreply.github.com>
2019-12-01 18:01:34 -0500

    Add dparams to mkcert.sh

--- mkcert.sh
+++ mkcert.sh
@@ -7,3 +7,4 @@ CERTNAME="${1:-server}"
 
 cd "$PRIVATE_DIR" && \
 openssl req -newkey rsa -nodes -keyout "$CERTNAME.key" -x509 -days 365 -out "$CERTNAME.pem" -subj "/CN=$HOSTNAME"
+openssl dhparam -out dhparams.pem 2048
